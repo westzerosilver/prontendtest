1926196d6cf5db0fbf5b02aba760203f974bb317
westzerosilver <oooiooop9988@gmail.com>
2025-03-02 00:12:55 +0900

    Chore: api 경로 변경 다시

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ const Login = () => {
   const onLoginClick = () => {
     // 소셜 로그인 페이지로 리디렉션
     // window.location.href = "http://localhost:8080/login/oauth2/code/google";
-    window.location.href = "https://likelion13th-swu.site/login/oauth2/code/google";
+    window.location.href = "https://api.likelion13th-swu.site/login/oauth2/code/google";
   };
 
   return (
